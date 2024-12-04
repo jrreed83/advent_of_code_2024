daa62ae2c587edf392e1cdbf1cb4a0ac25b7724b
jrreed83 <joeyrreed@gmail.com>
2024-12-03 20:54:31 -0800

    using some native javascript functions

--- day01/day1_p01.ts
+++ day01/day1_p01.ts
@@ -1,23 +1,27 @@
 import { parse } from "jsr:@std/csv/parse";
-import { unzip, mapNotNullish,zip,runningReduce } from "jsr:@std/collections";
+import { unzip, zip} from "jsr:@std/collections";
 
 
 const text = await Deno.readTextFile("input.txt");
 
-//const col_1 = [];
-//const col_2 = [];
+// Need a better way of defining the seperator ...
+const X = parse(text, { separator: "   "}).map(
+    function fn(x:[string, string]):[number,number] {
+        return [parseInt(x[0]), parseInt(x[1])];
+    }
+);
 
-const X = parse(text, { separator: "   "});
-const [x1, x2] = unzip(X);
-const col1 = mapNotNullish( x1, (x) => parseInt(x));
-const col2 = mapNotNullish( x2, (x) => parseInt(x));
+const [col1, col2] = unzip(X);
 
 col1.sort();
 col2.sort();
 
-const diffs = mapNotNullish(zip(col1, col2), ([a,b]) => Math.abs(a-b));
-const sums  = runningReduce(diffs, (sum, current) => sum + current, 0);
-const sum   = sums[sums.length-1];
+const sum = zip(col1, col2).reduce(
+    function fn(accum: number, pair: [number, number]) {
+        return accum + Math.abs(pair[0]-pair[1]);
+    },
+    0.0
+);
 
 console.log(`The sum is ${sum}`);
 
